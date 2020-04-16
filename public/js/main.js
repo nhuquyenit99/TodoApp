6f79da6d28cecb8348da736a971fb778237123a9
nhuquyenit99 <48923386+nhuquyenit99@users.noreply.github.com>
2020-04-16 17:21:24 +0700

    show all tasks

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,64 @@
-var storageKey = 'todoList';
+var storageKeyAll = 'allList';
+var storageKeyTodo = 'todoList';
+var storageKeyDone = 'doneList';
+
 var todoList = [];
+var allList = [];
+var doneList = [];
+
 var listString = '';
+var todoString = '';
+var doneString = '';
+
+var total = 0;
+
 function render(){
   var htmlList = document.getElementById('list-data');
-  listString = localStorage.getItem(storageKey);
-  todoList = JSON.parse(listString);
-  var content = todoList.map(function(item){
-    return  `<li>
-    <div class="view">
-    <input class="toggle" type="checkbox" ${item.done?'checked':''}>
-    <label>${item.content}</label>
-    <button class="destroy"></button>
-    </div>
-    <input class="edit" value="${item.content}">
-    </li>`;
-  });
-  htmlList.innerHTML = content.join('');
-  if (todoList.length!==0){
+  listString = localStorage.getItem(storageKeyAll);
+  if (listString){
+    allList = JSON.parse(listString);
+    count = allList.length;
+    var content = allList.map(function(item){
+      return  `<li>
+      <div class="view">
+      <input class="toggle" type="checkbox" ${item.done?'checked':''}>
+      <label>${item.content}</label>
+      <button class="destroy"></button>
+      </div>
+      <input class="edit" value="${item.content}">
+      </li>`;
+    });
+    htmlList.innerHTML = content.join('');
+
     var footerActions = document.getElementById('footer-actions');
     footerActions.style.display = "block";
+
+    var countTasks = document.getElementById('count-tasks');
+    todoString = localStorage.getItem(storageKeyTodo);
+    todoList = JSON.parse(todoString);
+    countTasks.innerHTML = '<strong>'+ todoList.length +'</strong> items left';
   }
-  var countTasks = document.getElementById('count-tasks');
-  countTasks.innerHTML = '<strong>'+ todoList.length +'</strong> items left'
 }
-function Assignment(content){
-    this.content = content;
-    this.done = false;
-    this.id = ++total;
+
+function Task(content){
+  this.content = content;
+  this.done = false;
+  this.id = ++total;
 }
-function addAssignment(e){
+
+function addTask(e){
   todoInput = document.getElementById('todo-input');
   if(e.charCode === 13 && todoInput.value!==''){
-    let newAssignment = new Assignment(todoInput.value);
-    todoList.push(newAssignment);
+    let newTask = new Task(todoInput.value);
+    todoList.push(newTask);
+    allList.push(newTask);
+
     todoInput.value ='';
-    listString = JSON.stringify(todoList);
-    localStorage.setItem(storageKey,listString);
+
+    ListString = JSON.stringify(allList);
+    localStorage.setItem(storageKeyAll,ListString);
+    todoString = JSON.stringify(todoList);
+    localStorage.setItem(storageKeyTodo,todoString);
   }
 
   render();
