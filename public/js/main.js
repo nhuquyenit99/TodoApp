06af22830e8c59ec6f13f5450e10c6ffe4a3a127
nhuquyenit99 <nhimhana1404@gmail.com>
2020-05-16 16:36:19 +0700

    fix HTT-5: rename variables and format code

--- public/js/main.js
+++ public/js/main.js
@@ -1,105 +1,104 @@
-var storageKey = 'allList';
+var storageKey = "all-list";
 
 var todoList = [];
-var allList = [];
-var doneList = [];
 
-var listString = '';
+var listString = "";
 
-function convertToHTML(list){
-  var content = allList.map(function(item){
-    return  `
-      <li id= ${item.id} class=${item.done?'completed':''}>
-        <div class="view">
-          <input class="toggle" type="checkbox" ${item.done?'checked':''}>
+function convertToHTML(list) {
+  var content = todoList.map(function (item) {
+    return `
+      <li id = ${item.id} class = ${item.done ? "completed" : ""}>
+        <div class = "todo-wrapper">
+          <input class = "toggle" type = "checkbox" ${
+            item.done ? "checked" : ""
+          }>
           <label>${item.content}</label>
-          <button class="destroy"></button>
+          <button class = "destroy"></button>
         </div>
-        <input class="edit" value="${item.content}">
+        <input class = "edit" value = "${item.content}">
       </li>`;
   });
   return content;
 }
-function showFooter(){
-  var element = document.getElementById('footer-actions');
-  if (allList.length!==0){
-    element.style.display = 'block';
-    showCountTasks();   
-  } 
-  else element.style.display = 'none';
+function showFooter() {
+  var element = document.getElementById("footer-actions");
+  if (todoList.length !== 0) {
+    element.style.display = "block";
+    showCountTasks();
+  } else element.style.display = "none";
 }
-function showCountTasks(){
-  var countTasks = document.getElementById('count-tasks');
-    todoList = allList.filter(item => item.done===false)
-    countTasks.innerHTML = '<strong>'+ todoList.length +'</strong> items left';
+function showCountTasks() {
+  var countTasks = document.getElementById("count-tasks");
+  var todoTasks = todoList.filter((item) => item.done === false);
+  countTasks.innerHTML = "<strong>" + todoTasks.length + "</strong> items left";
 }
-function render(){
-  var htmlList = document.getElementById('list-data');
+function render() {
+  var htmlList = document.getElementById("list-data");
   listString = localStorage.getItem(storageKey);
-  if (listString){
-    allList = JSON.parse(listString);
-    var content = convertToHTML(allList);
-    htmlList.innerHTML = content.join('');
+  if (listString) {
+    todoList = JSON.parse(listString);
+    var content = convertToHTML(todoList);
+    htmlList.innerHTML = content.join("");
   }
   showFooter();
 }
 
 // add new task
-var todoInput = document.getElementById('todo-input')
-todoInput.onkeypress = function (e){
+var todoInput = document.getElementById("todo-input");
+todoInput.onkeypress = function (e) {
   const enterKey = 13;
-  todoInput = document.getElementById('todo-input');
-  if(e.charCode === enterKey && todoInput.value.trim()!==''){
+  todoInput = document.getElementById("todo-input");
+  if (e.charCode === enterKey && todoInput.value.trim() !== "") {
     let newTask = {};
     newTask.id = new Date().valueOf();
     newTask.content = todoInput.value;
     newTask.done = false;
-    allList.push(newTask);
+    todoList.push(newTask);
 
-    todoInput.value ='';
+    todoInput.value = "";
 
-    ListString = JSON.stringify(allList);
-    localStorage.setItem(storageKey,ListString);
+    listString = JSON.stringify(todoList);
+    localStorage.setItem(storageKey, listString);
   }
   render();
-}
+};
 
-function deleteTask(taskId){
+function deleteTask(taskId) {
   var index;
-  for(let i=0;i<allList.length;i++){
-    if(allList[i].id === taskId) {
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i].id == taskId) {
       index = i;
-    } 
+    }
   }
-  allList.splice(index,1);
-  listString = JSON.stringify(allList);
-  localStorage.setItem(storageKey,listString);
+  todoList.splice(index, 1);
+  listString = JSON.stringify(todoList);
+  localStorage.setItem(storageKey, listString);
   render();
 }
 
-function completeTask(taskId){
-  allList.map(item=>{
-    if(item.id == taskId){
+function completeTask(taskId) {
+  todoList.map((item) => {
+    if (item.id == taskId) {
       item.done = !item.done;
       return item;
     }
     return item;
-  })
-  listString = JSON.stringify(allList);
-  localStorage.setItem(storageKey,listString);
+  });
+  listString = JSON.stringify(todoList);
+  localStorage.setItem(storageKey, listString);
 
   render();
 }
 
-var todoList = document.getElementById("list-data");
-todoList.addEventListener("click",event=>{
+var listData = document.getElementById("list-data");
+listData.addEventListener("click", (event) => {
   var element = event.target;
   var value = element.getAttribute("class");
-  switch (value){
+  switch (value) {
     case "destroy":
       deleteTask(element.parentNode.parentNode.id);
       break;
-    case "toggle": 
+    case "toggle":
       completeTask(element.parentNode.parentNode.id);
       break;
   }
